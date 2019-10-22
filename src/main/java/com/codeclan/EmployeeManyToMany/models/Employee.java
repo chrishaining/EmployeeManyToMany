577ac618b38eccffefd6874be907b3ef9ae523ad
Chris Haining <chrishaining@yahoo.co.uk>
2019-10-22 13:12:09 +0100

    can add employee to project

--- src/main/java/com/codeclan/EmployeeManyToMany/models/Employee.java
+++ src/main/java/com/codeclan/EmployeeManyToMany/models/Employee.java
@@ -3,6 +3,8 @@ package com.codeclan.EmployeeManyToMany.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "employees")
@@ -29,12 +31,34 @@ public class Employee {
     )
     private Department department;
 
+    @JsonIgnoreProperties("employees")
+    @ManyToMany
+    @JoinTable(
+            name = "employees_projects",
+            joinColumns = {
+                    @JoinColumn(
+                            name = "employee_id",
+                            nullable = false,
+                            updatable = false
+                    )
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(
+                            name = "project_id",
+                            nullable = false,
+                            updatable = false
+                    )
+            }
+    )
+    private List<Project> projects;
+
 
     public Employee(String firstName, String lastName, String employeeNumber, Department department) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.employeeNumber = employeeNumber;
         this.department = department;
+        this.projects = new ArrayList<>();
     }
 
     public Employee() {
@@ -79,5 +103,13 @@ public class Employee {
 
     public void setDepartment(Department department) {
         this.department = department;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
     }
 }
--- src/test/java/com/codeclan/EmployeeManyToMany/EmployeeManyToManyApplicationTests.java
+++ src/test/java/com/codeclan/EmployeeManyToMany/EmployeeManyToManyApplicationTests.java
@@ -2,8 +2,10 @@ package com.codeclan.EmployeeManyToMany;
 
 import com.codeclan.EmployeeManyToMany.models.Department;
 import com.codeclan.EmployeeManyToMany.models.Employee;
+import com.codeclan.EmployeeManyToMany.models.Project;
 import com.codeclan.EmployeeManyToMany.repositories.DepartmentRepository;
 import com.codeclan.EmployeeManyToMany.repositories.EmployeeRepository;
+import com.codeclan.EmployeeManyToMany.repositories.ProjectRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,16 +21,27 @@ class EmployeeManyToManyApplicationTests {
 	@Autowired
 	DepartmentRepository departmentRepository;
 
+	@Autowired
+	ProjectRepository projectRepository;
+
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	public void createEmployeeAndDepartment() {
+	public void createEmployeeAndDepartmentAndProjet() {
+
+		Project project1 = new Project("Personal Training", 3);
+		Project project2 = new Project("Personal Training", 3);
 		Department department = new Department("HR");
-		Employee employee = new Employee("Susan", "Jones", "NE345", department);
+		Employee employee1 = new Employee("Susan", "Jones", "NE345", department);
+
+		projectRepository.save(project1);
 		departmentRepository.save(department);
-		employeeRepository.save(employee);
+		employeeRepository.save(employee1);
+
+		project1.addEmployee(employee1);
+		projectRepository.save(project1);
 	}
 
 }
